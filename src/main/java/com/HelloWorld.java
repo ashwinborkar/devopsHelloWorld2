9f960933e6fabe6411eb5931b48a0175bfe97a26
ashwinborkar <ashwinborkar185@gmail.com>
2019-02-05 13:01:39 +0530

    added add func

--- src/main/java/com/HelloWorld.java
+++ src/main/java/com/HelloWorld.java
@@ -5,6 +5,14 @@ public class HelloWorld {
     public void printName(){
 
     System.out.println("This is my first java code to test jenkins CI/CD flow");
-    
+
+    }
+
+    public void add()
+    {
+        int a=10, b=20, c;
+        c=a+b;
+        System.out.println(+c);
+
     }
 }
